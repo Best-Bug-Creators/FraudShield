d1215b657428c0b48438539b23a918981aeb9568
Araujo Ana Rebeca Marques <n919431@gruposantander.com>
2023-03-28 17:39:00 -0300

    <fix> endpoints to analyses

--- ms-anti-fraud/src/routes/analysisRoute.js
+++ ms-anti-fraud/src/routes/analysisRoute.js
@@ -5,11 +5,11 @@ import analysisController from '../controllers/analysisController.js';
 const router = express.Router();
 
 router
-  .get('/analysis', analysisController.findAllAnalysis)
-  .get('/analysis/:id', analysisController.findAnalysisById)
-  .get('/analysis/under-review', analysisController.findAnalysisUnderReview)
-  .post('/analysis', analysisController.createAnalysis)
-  .patch('/analysis/:id', statusValidation, analysisController.updateAnalysis)
-  .delete('/analysis/:id', analysisController.deleteAnalysis);
+  .get('/analyses', analysisController.findAllAnalysis)
+  .get('/analyses/:id', analysisController.findAnalysisById)
+  .get('/analyses/under-review', analysisController.findAnalysisUnderReview)
+  .post('/analyses', analysisController.createAnalysis)
+  .patch('/analyses/:id', statusValidation, analysisController.updateAnalysis)
+  .delete('/analyses/:id', analysisController.deleteAnalysis);
 
 export default router;
