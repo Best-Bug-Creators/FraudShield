6e0f68326afbd29d0dcaade7f578f4e529f9aba4
Adson Gomes Olivera <adsonprofessionalemail@gmail.com>
2023-03-28 09:29:51 -0300

    <feat> Created routes for transaction

---
+++ ms-transaction/src/routes/transactionRoute.js
@@ -0,0 +1,9 @@
+import express from 'express';
+// import analysisController from '../controllers/analysisController.js';
+
+const router = express.Router();
+
+router
+  .get('/transaction/:id');
+
+export default router;
