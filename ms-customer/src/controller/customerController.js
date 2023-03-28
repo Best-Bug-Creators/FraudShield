cf74e31cd102b47130fe55bea90082aaace3b3ee
Matheus Leme <matheuslemelhp@gmail.com>
2023-03-28 09:27:12 -0300

    <fix> Fix try catch on customerController

--- ms-customer/src/controller/customerController.js
+++ ms-customer/src/controller/customerController.js
@@ -1,23 +1,22 @@
 import Customer from '../models/Customer.js';
 
 class CustomerController {
-  static getCustomerById = (req, res) => {
+  static getCustomerById = async (req, res) => {
     const { id } = req.params;
 
-    Customer.findById(id, (err, customer) => {
-      try {
-        if (!customer) {
-          res.status(404).send({ message: 'Customer not found. ' });
-        } else {
-          res.status(200).send({
-            personalData: customer.personalData,
-            address: customer.address,
-          });
-        }
-      } catch (error) {
-        res.status(500).send({ message: error.message });
+    try {
+      const customer = await Customer.findById(id);
+      if (!customer) {
+        res.status(404).send({ message: 'Customer not found. ' });
+      } else {
+        res.status(200).send({
+          personalData: customer.personalData,
+          address: customer.address,
+        });
       }
-    });
+    } catch (err) {
+      res.status(500).send({ message: err.message });
+    }
   };
 }
 
