05395327568a6e5c3bebcf26acd0ea68167e8e2f
Adson Gomes Olivera <adsonprofessionalemail@gmail.com>
2023-03-28 09:34:40 -0300

    <feat> Added status and response to endpoint controller

--- ms-transaction/src/controllers/transactionController.js
+++ ms-transaction/src/controllers/transactionController.js
@@ -1,8 +1,9 @@
 import TransactionService from '../services/transactionService';
+import HTTPStatus from '../helpers/HTTP.status';
 
 export const getById = async (req, res) => {
   const { id } = req.params;
   const response = await TransactionService.getById(id);
 
-  return res.status().json();
+  return res.status(HTTPStatus.OK).json(response);
 }
