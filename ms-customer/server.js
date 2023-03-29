d8545e1bc9d5caf6eb434dafa267c4f7eadef419
Matheus Leme <matheuslemelhp@gmail.com>
2023-03-29 10:44:17 -0300

    <fix> Connection string and port

--- ms-customer/server.js
+++ ms-customer/server.js
@@ -1,13 +1,11 @@
 /* eslint-disable no-console */
-import dotenv from 'dotenv';
 import app from './src/app.js';
 import db from './src/config/dbConnect.js';
-
-dotenv.config();
+import {} from 'dotenv/config';
 
 db.on('error', console.log.bind(console, 'Database connection error.'));
 db.once('open', () => console.log('Connection to the database successfully completed.'));
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3002;
 
 app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
