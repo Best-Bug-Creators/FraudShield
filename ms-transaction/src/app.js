534c0f68ee3f6cb40aa0de39f1e021bad9e361a6
Adson Gomes Olivera <adsonprofessionalemail@gmail.com>
2023-03-29 22:45:28 -0300

    <setup> Adding support to assert in import for eslint

--- ms-transaction/src/app.js
+++ ms-transaction/src/app.js
@@ -5,7 +5,7 @@ import swaggerUiExpress from 'swagger-ui-express';
 import db from './config/dbConfig.js';
 import routes from './routes/index.js';
 import errorMiddleware from './middleware/error.middleware.js';
-import swaggerDocument from '../swagger/transaction-swagger.json';
+import swaggerDocument from '../swagger/transaction-swagger.json' assert { type: 'json' };
 
 db.on('error', console.log.bind(console, 'Connection error'));
 db.once('open', () => console.log('Database connection was successful'));
