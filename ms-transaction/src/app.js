50f458f6142eb58981727807cc23dc5e6b82d850
Adson Gomes Olivera <adsonprofessionalemail@gmail.com>
2023-03-29 22:33:40 -0300

    <doc> API swagger docs exposed on /api-docs route

--- ms-transaction/src/app.js
+++ ms-transaction/src/app.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-console */
 import 'express-async-errors';
 import express from 'express';
+import swaggerUiExpress from 'swagger-ui-express';
 import db from './config/dbConfig.js';
 import routes from './routes/index.js';
 import errorMiddleware from './middleware/error.middleware.js';
+import swaggerDocument from '../swagger/transaction-swagger.json';
 
 db.on('error', console.log.bind(console, 'Connection error'));
 db.once('open', () => console.log('Database connection was successful'));
@@ -13,5 +15,8 @@ app.use(express.json());
 
 routes(app);
 app.use(errorMiddleware);
+
+app.use('/api-docs', swaggerUiExpress.serve);
+app.use('/api-docs', swaggerUiExpress.setup(swaggerDocument));
 
 export default app;
